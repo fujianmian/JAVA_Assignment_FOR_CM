803b3f7054b93200662518dbe3eb4198ac0d2b7d
fujianmian <144903122+fujianmian@users.noreply.github.com>
2024-08-21 02:37:32 +0800

    register and login design

--- src/main/java/com/mycompany/java_assignemt_1/JAVA_ASSIGNEMT_1.java
+++ src/main/java/com/mycompany/java_assignemt_1/JAVA_ASSIGNEMT_1.java
@@ -12,5 +12,9 @@ public class JAVA_ASSIGNEMT_1 {
 
     public static void main(String[] args) {
         System.out.println("123adawdadawdawd");
+        
+        
+        User_Login userLoginFrame = new User_Login();
+        userLoginFrame.setVisible(true);
     }
 }
